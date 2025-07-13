6437d81b2051e6f6ca468b4d70eb5bf500007f55
BhupeshB7 <bhupeshkr2912@gmail.com>
2025-07-13 13:08:47 +0530

    update

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import cors from "cors";
 import spotifyRoutes from "./routes/spotify.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: "public" });
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // app.get("/", (req, res) => res.send("Welcome to the Cactro Spotify API"));
